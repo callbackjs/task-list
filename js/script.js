04e233ee4921627ffc94c9327aaf56a9de1923f6
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-29 19:26:07 +0530

    Add improved files.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 (function(window, document, undefined) {
-
   var ENTER_KEY_CODE = 13;
+
+  var taskTemplate = document.getElementById('task-template');
+  var renderTaskTemplate = Handlebars.compile(taskTemplate.innerHTML);
 
   var taskInput = document.getElementById('task');
   var taskList = document.getElementById('task-list');
+
+  if (localStorage.tasks) {
+    taskList.innerHTML = localStorage.tasks;
+  }
 
   taskInput.addEventListener("keydown", function(event) {
     if (event.keyCode === ENTER_KEY_CODE) {
@@ -19,41 +25,36 @@
    * taskInput -- the HTMLElement input tag
    */
   function addTaskToList() {
-    if (taskInput.value)  {
+    if (taskInput.value) {
       var li = document.createElement('li');
-      li.textContent = taskInput.value;
-      li.innerHTML += " <a href=\"#\" class=\"delete\">&#215;</a> <a href=\"#\" class=\"check\">&#9744;</a>";
+      li.innerHTML = renderTaskTemplate({ task: taskInput.value });
 
-      addTaskListeners(li);
       taskInput.value = "";
       taskList.appendChild(li);
+      localStorage.tasks = taskList.innerHTML;
     }
   }
 
-  /* Handles check/delete events for the given task.
-   *
-   * Arguments:
-   * taskLi -- the HTMLElement li tag
-   */
-  function addTaskListeners(li) {
-    var checkbox = li.querySelector('.check');
-    checkbox.addEventListener('click', function(event) {
-      event.preventDefault();
+  taskList.addEventListener('click', function(event) {
+    var target = event.target;
+    var li = target.parentNode;
 
+    if (target.classList.contains('check')) {
+      event.preventDefault();
       if (li.classList.contains('checked')) {
         li.classList.remove('checked');
-        checkbox.innerHTML = '&#9744;';
+        target.innerHTML = '&#9744;';
       } else {
         li.classList.add('checked');
-        checkbox.innerHTML = '&#9745;';
+        target.innerHTML = '&#9745;';
       }
-    });
 
-    var deleteButton = li.querySelector('.delete');
-    deleteButton.addEventListener('click', function(event) {
+      localStorage.tasks = taskList.innerHTML;
+    } else if (target.classList.contains("delete")) {
       event.preventDefault();
       li.parentNode.removeChild(li);
-    });
-  }
+      localStorage.tasks = taskList.innerHTML;
+    }
+  });
 
 })(this, this.document);
