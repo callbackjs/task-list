1b75c3ff7872363e7a40095e71dcd7f6dae276d5
Vivek Nair <vnair611@stanford.edu>
2016-01-18 23:06:56 -0800

    Remove the IIFE

--- js/script.js
+++ js/script.js
@@ -1,57 +1,53 @@
-(function(window, document, undefined) {
+var ENTER_KEY_CODE = 13;
 
-  var ENTER_KEY_CODE = 13;
+var taskInput = document.getElementById('task');
+var taskList = document.getElementById('task-list');
 
-  var taskInput = document.getElementById('task');
-  var taskList = document.getElementById('task-list');
+taskInput.addEventListener("keydown", function(event) {
+  if (event.keyCode === ENTER_KEY_CODE) {
+    event.preventDefault();
+    addTaskToList();
+  }
+});
 
-  taskInput.addEventListener("keydown", function(event) {
-    if (event.keyCode === ENTER_KEY_CODE) {
-      event.preventDefault();
-      addTaskToList();
+/* Adds a new task item to the list, with the value from the
+ * given input.
+ *
+ * Arguments:
+ * taskInput -- the HTMLElement input tag
+ */
+function addTaskToList() {
+  if (taskInput.value)  {
+    var li = document.createElement('li');
+    li.textContent = taskInput.value;
+    li.innerHTML += " <a href=\"#\" class=\"delete\"></a> <a href=\"#\" class=\"check\"></a>";
+
+    addTaskListeners(li);
+    taskInput.value = "";
+    taskList.appendChild(li);
+  }
+}
+
+/* Handles check/delete events for the given task.
+ *
+ * Arguments:
+ * taskLi -- the HTMLElement li tag
+ */
+function addTaskListeners(li) {
+  var checkbox = li.querySelector('.check');
+  checkbox.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (li.classList.contains('checked')) {
+      li.classList.remove('checked');
+    } else {
+      li.classList.add('checked');
     }
   });
 
-  /* Adds a new task item to the list, with the value from the
-   * given input.
-   *
-   * Arguments:
-   * taskInput -- the HTMLElement input tag
-   */
-  function addTaskToList() {
-    if (taskInput.value)  {
-      var li = document.createElement('li');
-      li.textContent = taskInput.value;
-      li.innerHTML += " <a href=\"#\" class=\"delete\"></a> <a href=\"#\" class=\"check\"></a>";
-
-      addTaskListeners(li);
-      taskInput.value = "";
-      taskList.appendChild(li);
-    }
-  }
-
-  /* Handles check/delete events for the given task.
-   *
-   * Arguments:
-   * taskLi -- the HTMLElement li tag
-   */
-  function addTaskListeners(li) {
-    var checkbox = li.querySelector('.check');
-    checkbox.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      if (li.classList.contains('checked')) {
-        li.classList.remove('checked');
-      } else {
-        li.classList.add('checked');
-      }
-    });
-
-    var deleteButton = li.querySelector('.delete');
-    deleteButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      li.parentNode.removeChild(li);
-    });
-  }
-
-})(this, this.document);
+  var deleteButton = li.querySelector('.delete');
+  deleteButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    li.parentNode.removeChild(li);
+  });
+}
