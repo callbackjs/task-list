3c9a2d4b4be1b0b8114d0aa4edf2d2847f86a23b
Karthik Viswanathan <karthik.ksv@gmail.com>
2016-10-12 21:19:52 -0700

    Convert to ES6.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-var ENTER_KEY_CODE = 13;
+const ENTER_KEY_CODE = 13
 
-var taskInput = document.getElementById('task');
-var taskList = document.getElementById('task-list');
+const taskInput = document.getElementById('task')
+const taskList = document.getElementById('task-list')
 
-taskInput.addEventListener("keydown", function(event) {
+taskInput.addEventListener("keydown", event => {
   if (event.keyCode === ENTER_KEY_CODE) {
-    event.preventDefault();
-    addTaskToList();
+    event.preventDefault()
+    addTaskToList()
   }
-});
+})
 
 /* Adds a new task item to the list, with the value from the
  * given input.
@@ -18,13 +18,13 @@ taskInput.addEventListener("keydown", function(event) {
  */
 function addTaskToList() {
   if (taskInput.value)  {
-    var li = document.createElement('li');
-    li.textContent = taskInput.value;
-    li.innerHTML += " <a href=\"#\" class=\"delete\"></a> <a href=\"#\" class=\"check\"></a>";
+    const li = document.createElement('li')
+    li.textContent = taskInput.value
+    li.innerHTML += " <a href=\"#\" class=\"delete\"></a> <a href=\"#\" class=\"check\"></a>"
 
-    addTaskListeners(li);
-    taskInput.value = "";
-    taskList.appendChild(li);
+    addTaskListeners(li)
+    taskInput.value = ""
+    taskList.appendChild(li)
   }
 }
 
@@ -34,20 +34,20 @@ function addTaskToList() {
  * taskLi -- the HTMLElement li tag
  */
 function addTaskListeners(li) {
-  var checkbox = li.querySelector('.check');
-  checkbox.addEventListener('click', function(event) {
-    event.preventDefault();
+  const checkbox = li.querySelector('.check')
+  checkbox.addEventListener('click', event => {
+    event.preventDefault()
 
     if (li.classList.contains('checked')) {
-      li.classList.remove('checked');
+      li.classList.remove('checked')
     } else {
-      li.classList.add('checked');
+      li.classList.add('checked')
     }
-  });
+  })
 
-  var deleteButton = li.querySelector('.delete');
-  deleteButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    li.parentNode.removeChild(li);
-  });
+  const deleteButton = li.querySelector('.delete')
+  deleteButton.addEventListener('click', event => {
+    event.preventDefault()
+    li.parentNode.removeChild(li)
+  })
 }
